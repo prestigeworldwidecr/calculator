4369605c107658e21ecd59bc1256ffaad683eb18
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2023-02-08 08:12:52 -0600

    msg1

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 // app.js
+
+/*
+
+1. input content, no error checking for the time being
+2. place on stack
+3. pop numbers, operators
+4. calculate in switch/case block
+
+*/
+
 const prompt = require('prompt-sync')();
 
 const s = prompt('How ya doing? ');
