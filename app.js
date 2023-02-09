d7d2cc3272acf5dc48d306b9f25f28dc179abfa7
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2023-02-08 21:05:46 -0600

    msg1

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // app.js
 
+const { parse } = require('pegjs/lib/parser');
+
 /*
 
-1. input content, no error checking for the time being -- main
-.. don't need to, store the elements into an array XX 2. place on stack -- parseUserInput, can do w/linkedList || ArrayList
+1. input content, no error checking for the time being -- init
+2. place on stack -- parseUserInput
 3. pop numbers, operators -- stackUserInput
 4. calculate in switch/case block -- calculate
 
@@ -22,7 +24,92 @@ function init ()
 
 function parseUserInput(s)
 {
+    for (let i = 0; i < s.length; i++)
+    {
+        let tmp = s[i];
+        console.log('!0', i, tmp, s);
     
+        switch(tmp)
+        {
+            case '+':
+                console.log('!1', tmp, i, s);
+                break;
+            case '-':
+                console.log('!2', tmp, i, s);
+                break;
+            case '/':
+                console.log('!3', tmp, i, s);
+                break;
+            case '*':
+                console.log('!4', tmp, i, s);   
+                break;
+            default:
+                tmp1 = parseInt(s);
+            
+                if (Number.isInteger (tmp1))
+                {
+                    s = s.substr(i + tmp1.toString().length, s.length);
+                    // i = i + tmp1.toString().length;
+                    i = 0;
+                    console.log('!5', tmp1, i, s, s[i]);
+                }
+                
+                else
+                {
+                    console.log('!6', tmp, i, s);   
+                }
+
+                break;         
+        }
+    }
+
+    // 66+6
+    // 0123
+    /*
+
+    /*
+        
+        let tmp = parseInt(s);
+        
+        if (Number.isInteger (tmp))
+        {
+            i = i + tmp.toString().length;
+            let j = s.length;
+            s = s.substr(i, j);
+            console.log('! ', tmp, i, j, s);
+        }
+
+        else
+        {
+            console.log('* ', tmp);
+        }
+
+    // let exp = [];
+
+    // console.log (Number.isInteger (parseInt(s)));
+
+    let tmp = parseInt(s);
+    
+    switch(tmp)
+    {
+        case '+':
+            console.log(tmp);
+            break;
+        case '-':
+            console.log(tmp);
+            break;
+        case '/':
+            console.log(tmp);
+            break;
+        case '*':
+            console.log(tmp);   
+            break;
+        default:
+            console.log(tmp);   
+            break;         
+    }
+
+    */
 }
 
 /*
